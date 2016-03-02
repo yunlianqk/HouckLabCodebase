daabd42da09237847e61db9e9f6c6f1be93c0a49
houcklab <houcklabprinceton@gmail.com>
2016-03-02 16:53:17 -0500

    James Oxford commit

--- test_script.m
+++ test_script.m
@@ -10,8 +10,7 @@ yoko.rampinterval = 0.01;
 yoko.voltage = 0;
 yoko.SetVoltage();
 %% Set triggen generator
-triggen.period = 30e-6;
-triggen.SetParams();
+triggen.SetPeriod(30e-6);
 
 %% Transmission scan with PNAX
 transparams.start = 6.25087e9;
@@ -20,16 +19,31 @@ transparams.points = 3001;
 transparams.power = -60;
 transparams.averages = 65536;
 transparams.ifbandwidth = 3e3;
-
+% Amp trace
+transparams.trace = 1;
+transparams.meastype = 'S21';
+transparams.format = 'MLOG';
 pnax.transparams = transparams;
 pnax.SetTransParams();
-% pause(10);
-% trans = pnax.Read();
-% freqvector = pnax.GetAxis();
-% figure(1);
-% plot(freqvector/1e9, trans);
-% xlabel('Frequency (GHz)');
-% ylabel('Transmission (GHz)');
+% Phase trace
+pnax.transparams.trace = 2;
+pnax.transparams.format = 'UPH';
+pnax.SetTransParams();
+% Read data
+pause(5);
+pnax.SetActiveTrace(1);
+transamp = pnax.Read();
+pnax.SetActiveTrace(2);
+transph = pnax.Read();
+freqvector = pnax.GetAxis();
+figure(1);
+subplot(2,1,1);
+plot(freqvector/1e9, transamp);
+title('Amplitude');
+subplot(2,1,2);
+plot(freqvector/1e9, transph);
+title('Phase');
+xlabel('Frequency (GHz)');
 %% Spec scan with PNAX
 specparams.start = 8.42e9;
 specparams.stop = 8.44e9;
@@ -39,28 +53,45 @@ specparams.averages = 65536;
 specparams.ifbandwidth = 3e3;
 specparams.cwfreq = 6.2558e9;
 specparams.cwpower = -55;
-
+% Amp trace
+specparams.trace = 3;
+specparams.meastype = 'S21';
+specparams.format = 'MLOG';
 pnax.specparams = specparams;
 pnax.SetSpecParams();
-% pause(10);
-% spec = pnax.Read();
-% freqvector = pnax.GetAxis();
-% figure(2);
-% plot(freqvector/1e9, spec);
-% xlabel('Spec frequency (GHz)');
-% ylabel('Transmission (GHz)');
+% Phase trace
+specparams.trace = 4;
+specparams.format = 'UPH';
+pnax.specparams = specparams;
+pnax.SetSpecParams();
+% Read data
+pause(5);
+pnax.SetActiveTrace(3);
+specamp = pnax.Read();
+pnax.SetActiveTrace(4);
+specph = pnax.Read();
+freqvector = pnax.GetAxis();
+figure(2);
+subplot(2,1,1);
+plot(freqvector/1e9, specamp);
+title('Amplitude');
+subplot(2,1,2);
+plot(freqvector/1e9, specph);
+title('Phase');
+xlabel('Spec frequency (GHz)');
+
 %% Test AWG
 taxis = 0:0.8e-9:30e-6;
 % Sin wave for channel 1
-waveform1 = sin(2*pi*1e6*taxis).*(taxis <= 10e-6);
+waveform1 = 2*sin(2*pi*1e6*taxis).*(taxis <= 10e-6);
 % Gaussian pulse for channel 2
 % waveform2 = exp(-(taxis-5e-6).^2/(2*0.5e-6^2));
-waveform2 = exp(-(taxis-5e-6).^2/(2*0.1e-6^2)) + exp(-(taxis-6e-6).^2/(2*0.1e-6^2));
+waveform2 = 3*exp(-(taxis-5e-6).^2/(2*0.1e-6^2)) + exp(-(taxis-6e-6).^2/(2*0.1e-6^2));
 pulsegen.timeaxis = taxis;
 pulsegen.waveform1 = waveform1;
 pulsegen.waveform2 = waveform2;
 pulsegen.SetParams();
-%%
+% Plot waveforms
 figure(3);
 subplot(2,1,1);
 hold off;
