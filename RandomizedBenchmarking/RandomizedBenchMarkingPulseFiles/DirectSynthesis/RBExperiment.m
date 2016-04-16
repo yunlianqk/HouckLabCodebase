9e21042ca131d08d97a15e244452f5ebcc86171c
jjraftery <jjraftery@gmail.com>
2016-04-16 16:10:01 -0400

    JJR - Refactoring Randomized Benchmarking Code.  Now the RBExperiment object will have object arrays corresponding to primitive gates, clifford gates, and sequences.  These will be hardware agnostic.

--- RandomizedBenchmarking/RandomizedBenchMarkingPulseFiles/DirectSynthesis/RBExperiment.m
+++ RandomizedBenchmarking/RandomizedBenchMarkingPulseFiles/DirectSynthesis/RBExperiment.m
@@ -3,20 +3,23 @@ classdef RBExperiment < handle
     %be sent to the awg.
     
     properties
+        primitives; % empty object array of primitive gates. populated in constructor
+        cliffords; % empty object array of clifford gates
+        
         % sequence properties
 %         sequenceList = 1:10; % list containing number of clifford gates in each subsequence
         sequenceList = 4; % list containing number of clifford gates in each subsequence
-        primitiveLists; % cell array containing lists of the gate primitives for each subsequence
+%         primitiveLists; % cell array containing lists of the gate primitives for each subsequence
         
         % general waveform properties
-        samplingRate = 1.25e9; % sampling rate to generate baseband signals - 1.25 GS/s works with M9330AWG 
-        startDelay = .1e-6; % delay after start of waveform before pulses start   
-        endDelay = .1e-6; % delay after end of measurement pulse to end of waveform
+%         samplingRate = 1.25e9; % sampling rate to generate baseband signals - 1.25 GS/s works with M9330AWG 
+%         startDelay = .1e-6; % delay after start of waveform before pulses start   
+%         endDelay = .1e-6; % delay after end of measurement pulse to end of waveform
         
         % qubit pulse properties
-        gateSigma = 10e-9; % gaussian standard deviation
+%         gateSigma = 10e-9; % gaussian standard deviation
 %         gateCutoff = 4*gateSigma; % total time gate is nonzero, centered around pulse peak
-        gateBuffer = 10e-9; % extra time between gates, 0 means next pulse starts as soon as previous one gets cut off
+%         gateBuffer = 10e-9; % extra time between gates, 0 means next pulse starts as soon as previous one gets cut off
         
         % measurement pulse properties
         % ADC card trigger
@@ -26,11 +29,35 @@ classdef RBExperiment < handle
 %     properties (Dependent)
     
     methods
-        function generatePrimitives(obj) % use provided IBM code to generate cell array of lists
-            primitiveLists = CliffordGroup(obj.sequenceList);
-            size(primitiveLists)
-            obj.primitiveLists = primitiveLists;
-            obj.primitiveLists{1}
+        function obj=RBExperiment()% constructor
+            obj.initPrimitives();
+        end
+        
+        function obj=initPrimitives(obj)
+            primitives(1)=gaussianWithDrag('Identity',0,0);
+            primitives(2)=gaussianWithDrag('X180',0,pi);
+            primitives(3)=gaussianWithDrag('X90',0,pi/2);
+            primitives(4)=gaussianWithDrag('Xm90',0,-pi/2);
+            primitives(5)=gaussianWithDrag('Y180',pi/2,pi);
+            primitives(6)=gaussianWithDrag('Y90',pi/2,pi/2);
+            primitives(7)=gaussianWithDrag('Ym90',pi/2,-pi/2);
+            obj.primitives=primitives;
+            
+            
+%             'X90pPulse', 'Y90pPulse', 'X90mPulse', 'Y90mPulse','QIdPulse','XpPulse', 'YpPulse'
+            
+            
+            
+            
+            
+            % generate object array of cliffords
+
+        
+%         function generatePrimitives(obj) % use provided IBM code to generate cell array of lists
+%             primitiveLists = CliffordGroup(obj.sequenceList);
+% %             size(primitiveLists)
+%             obj.primitiveLists = primitiveLists;
+%             obj.primitiveLists{1}
         end
         
         % calculate peak times for the pulses in a subsequence waveform set
---
+++ RandomizedBenchmarking/RandomizedBenchMarkingPulseFiles/DirectSynthesis/gaussianWithDrag.m
@@ -0,0 +1,95 @@
+classdef gaussianWithDrag < handle
+    % Gate object.  Clifford gates are formed from a few of these primitives
+    % together.  This is a basic gaussian pulse with a drag pulse in the
+    % quadrature.  
+    
+    properties
+        name;
+        unitary; % unitary matrix of ideal gate
+        rotation; % pi, pi/2 etc.  amount of rotation in radians
+        azimuth; % angle in equator of bloch sphere and IQ plane.  0 corresponds to x rotation, pi/2 to y rotation
+        amplitude=1; % amplitude of main gaussian pulse
+        dragAmplitude=.5; % amplitude of drag pulse in quadrature
+        sigma = 10e-9; % gaussian width in seconds
+        cutoff = 40e-9; % force pulse tail to zero. this is the total time the pulse is nonzero in seconds
+        buffer = 10e-9; % extra time beyond the cutoff to separate gates.  this is the total buffer, so half before and half after.
+    end
+    
+    methods
+        function obj=gaussianWithDrag(name, azimuth, rotation)   % constructor takes in axis for the pulse (0 is x, pi/2 is y) and a rotation angle
+            obj.name=name;
+            obj.azimuth=azimuth;
+            obj.rotation=rotation;
+            
+            % calculate gate unitary
+            sm =[0 1; 0 0];
+            sp = sm';
+            sx = full(sp+sm);
+            sy = full(1i*sp-1i*sm);
+            sz = [1 0;0 -1];
+            unitary=expm(-1i*rotation/2*(cos(azimuth)*sx+sin(azimuth)*sy));
+            obj.unitary=unitary;
+        end
+        
+        function draw(obj) % visualize - draw waveform and bloch vector
+            % print some text
+            fprintf(['Gate name: ' obj.name '\n'])
+            fprintf(['azimuth: ' num2str(obj.azimuth) '\n'])
+            fprintf(['rotation: ' num2str(obj.rotation) '\n'])
+            fprintf(['unitary rotation matrix:\n'])
+            disp(obj.unitary)
+            % create waveform time axis
+            pulseTime = obj.cutoff+obj.buffer; % calculate length of pulse
+            t = linspace(-pulseTime,pulseTime,1001); % make time axis twice as long as pulse 
+            % create gaussian
+            gaussian = obj.amplitude.*exp(-(t.^2)./(2.*obj.sigma.^2));
+            gaussianCutoff = gaussian.*(t>(-obj.cutoff/2)).*(t<(obj.cutoff/2));
+            % create drag
+            drag = (t./(obj.sigma.^2)).*exp(-(t.^2)./(2.*obj.sigma.^2));
+            drag = drag/max(drag); % normalize
+            drag = obj.dragAmplitude*drag;
+            dragCutoff = drag.*(t>(-obj.cutoff/2)).*(t<(obj.cutoff/2));
+            % find I and Q baseband using azimuth
+            iBaseband=cos(obj.azimuth).*gaussianCutoff+sin(obj.azimuth).*dragCutoff;
+            qBaseband=sin(obj.azimuth).*gaussianCutoff+cos(obj.azimuth).*dragCutoff;
+            % buffer
+            buffer = (t>-pulseTime/2).*(t<pulseTime/2);
+            % plot
+            figure(712);
+            subplot(3,2,1);
+            plot(t,gaussian,'b',t,gaussianCutoff,'r',t,buffer*max(gaussian),'k');
+            title('gaussian pulse')
+            subplot(3,2,3);
+            plot(t,drag,'b',t,dragCutoff,t,buffer*max(drag),'k');
+            title('drag pulse')
+            subplot(3,2,[2,4]);
+%             scatter3(iBaseband, qBaseband,t,'k.');
+            scatter3(iBaseband, qBaseband,t,[],1:length(t),'.');
+            axis square;
+            plotMax=max([max(iBaseband) max(qBaseband)]);tmax=max(t);
+            axis([-plotMax plotMax -plotMax plotMax -tmax tmax])
+            ax=subplot(3,2,6);
+            blochSpherePlot(ax,pi,obj.azimuth);
+            blochSpherePlot(ax,pi-obj.rotation,obj.azimuth,'replot');
+            subplot(3,2,5);
+            plot(t,iBaseband,'b',t,qBaseband,'r')
+            title('I and Q baseband waveforms')
+        end
+            
+    end
+end
+            
+        
+        
+
+        
+% list of the primitives        
+% QIdPulse = 'QId';
+% XpPulse = 'Xp';
+% YpPulse = 'Yp';
+% XmPulse = 'Xm';
+% YmPulse = 'Ym';
+% X90pPulse = 'X90p';
+% Y90pPulse = 'Y90p';
+% X90mPulse = 'X90m';
+% Y90mPulse = 'Y90m';
