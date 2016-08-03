8f43049b567716676ae3cdf9bb41272bb937ad82
jjraftery <jjraftery@gmail.com>
2016-08-03 15:44:28 -0400

    Now that acquisition is sorted out, updating the experiments in explib.

--- +explib/RabiExperiment.m
+++ +explib/RabiExperiment.m
@@ -3,16 +3,16 @@ classdef RabiExperiment < handle
 
     properties 
         % change these to tweak the experiment. 
-        qubitFreq=5e9;
+        qubitFreq=4.766e9;
         startAmp=1;
         stopAmp=0;
-        points = 11;
+        points = 101;
         gateType = 'X180';
         interPulseBuffer = 200e-9; % time between qubit pulse and measurement pulse
-        cavityFreq=7e9; % cavity frequency
+        cavityFreq=10.165e9; % cavity frequency
         measDuration = 5e-6;
         measStartTime = 5e-6; 
-        endBuffer = 50e-9; % buffer after measurement pulse
+        endBuffer = 5e-6; % buffer after measurement pulse
         samplingRate=32e9; % sampling rate
         % these are auto calculated
         ampVector
@@ -24,7 +24,7 @@ classdef RabiExperiment < handle
     end
     
     methods
-        function obj=RabiExperiment2()
+        function obj=RabiExperiment()
             % constructor generates the necessary objects and calculates the dependent parameters
             obj.qubit = pulselib.singleGate(obj.gateType);
             obj.measurement=pulselib.measPulse(obj.measDuration);
@@ -38,7 +38,9 @@ classdef RabiExperiment < handle
             w = paramlib.M8195A.waveset();
             tStep = 1/obj.samplingRate;
             t = 0:tStep:(obj.waveformEndTime);
-            ch2waveform = sin(2*pi*obj.cavityFreq*t); % lo waveform is always the same
+            loWaveform = sin(2*pi*obj.cavityFreq*t);
+            markerWaveform = ones(1,length(t)).*(t>10e-9).*(t<510e-9);
+            backgroundWaveform = zeros(1,length(t));
             for ind=1:obj.points
                 q=obj.qubit;
                 q.amplitude=obj.ampVector(ind);
@@ -49,17 +51,67 @@ classdef RabiExperiment < handle
                 iMeasMod=cos(2*pi*obj.cavityFreq*t).*iMeasBaseband;
                 qMeasMod=sin(2*pi*obj.cavityFreq*t).*qMeasBaseband;
                 ch1waveform = iQubitMod+qQubitMod+iMeasMod+qMeasMod;
-                s1=w.newSegment(ch1waveform);
+                s1=w.newSegment(ch1waveform,markerWaveform,[1 0; 0 0; 0 0; 0 0]);
                 p1=w.newPlaylistItem(s1);
-                % since s2 will be played simultaneously with s1, it does
-                % not need its own playlist item!
-                slo=w.newSegment(ch2waveform,[0 0; 1 0; 0 0; 0 0]);
-                % set lo to play simultaneously
-                slo.id = s1.id;
+                % create LO segment with same id to play simultaneously
+                s2=w.newSegment(loWaveform,markerWaveform,[0 0; 1 0; 0 0; 0 0]);
+                s2.id = s1.id;
+                % add background to playlist
+                sBack = w.newSegment(backgroundWaveform,markerWaveform,[1 0; 0 0; 0 0; 0 0]);
+                pBack = w.newPlaylistItem(sBack);
+                % add lo to play @ same time as background
+                s3=w.newSegment(loWaveform,markerWaveform,[0 0; 1 0; 0 0; 0 0]);
+                s3.id = sBack.id;
             end
             % last playlist item must have advance set to 'auto'
-            p1.advance='Auto';
+            pBack.advance='Auto';
         end
+        
+        function [tempI,tempQ] = runExperimentM8195A(obj,awg,card,cardparams)
+            % integration times
+            intStart=2000; intStop=5000;
+            % software averages
+            softavg=100;
+            w = obj.genWaveset_M8195A();
+            WaveLib = awg.WavesetExtractSegmentLibraryStruct(w);
+            PlayList = awg.WavesetExtractPlaylistStruct(w);
+%             w.drawSegmentLibrary()
+%             w.drawPlaylist()
+%             WaveLib = awg.ApplyCorrection(WaveLib);
+            awg.Wavedownload(WaveLib);
+            cardparams.segments=length(w.playlist);
+            cardparams.delaytime=obj.measStartTime-1e-6;
+            card.SetParams(cardparams);
+            tstep=1/card.params.samplerate;
+            taxis=(tstep:tstep:card.params.samples/card.params.samplerate)'./1e-6;%mus units
+            % READ
+            % intialize matrices
+            samples=uint64(cardparams.samples);
+            Idata=zeros(cardparams.segments/2,samples);
+            Qdata=zeros(cardparams.segments/2,samples);
+            Pdata=zeros(cardparams.segments/2,samples);
+            for i=1:softavg
+                % "hardware" averaged I,I^2 data
+                [tempI,tempI2,tempQ,tempQ2] = card.ReadIandQcomplicated(awg,PlayList);
+                % software acumulation
+                Idata=Idata+tempI;
+                Qdata=Qdata+tempQ;
+                Pdata=Pdata+tempI2+tempQ2;
+                Pint=mean(Pdata(:,intStart:intStop)');
+                
+                figure(101);
+                subplot(2,2,1); imagesc(taxis,obj.freqVector,Idata);title('In phase');ylabel('Frequency (GHz)');xlabel('Time (\mus)');
+                subplot(2,2,2); imagesc(taxis,obj.freqVector,Qdata);title('Quad phase');ylabel('Frequency (GHz)');xlabel('Time (\mus)');
+                subplot(2,2,3); imagesc(taxis,obj.freqVector,Pdata);title('Power I^2+Q^2');ylabel('Frequency (GHz)');xlabel('Time (\mus)');
+                subplot(2,2,4); plot(obj.freqVector,sqrt(Pint));ylabel('Power I^2+Q^2');xlabel('Frequency (GHz)');
+                pause(0.5);
+            end
+            Idata=Idata./softavg;
+            Qdata=Qdata./softavg;
+            Pdata=Pdata./softavg;
+            Pint=Pint./softavg;
+        end
+        
     end
 end
        
