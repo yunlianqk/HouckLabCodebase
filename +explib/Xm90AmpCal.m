6d9475f63ddfacfea22605f91eae2880aa06e3be
jjraftery <jjraftery@gmail.com>
2016-08-06 21:48:55 -0400

    JJR-cleanup plus y90 cal pulses

--- +explib/Xm90AmpCal.m
+++ +explib/Xm90AmpCal.m
@@ -1,20 +1,17 @@
-classdef X90AmpCal< handle
-    % Pi pulse error amplification. After an initial pi/2 pulse it will
+classdef Xm90AmpCal< handle
+    % minus Pi/2 pulse amplitude error amplification. After an initial pi/2 pulse it will
     % vary the number of subsequent pi pulses.  
     
     properties
         % change these to tweak the experiment
-        experimentName = 'X90AmpCal';
+        experimentName = 'Xm90AmpCal';
         numGateVector = 2:2:40;
         qubitFreq=4.772869998748302e9;
-%         qubitAmplitude = .74/2;
-%         iGateAmplitude = .74/2;
-        qubitAmplitude = .5/2;
-        iGateAmplitude = .5/2;
+        qubitAmplitude = .3575;
+        qubitDragAmplitude = .016;
         qubitSigma = 25e-9;
-        iGateSigma = 25e-9;
-        iGateType = 'X90'; % initial gate (currently just use a half power pi pulse... need to rethink how this should be done after calibrations!)
-        gateType = 'X90'; % repeated gate type 
+        iGateType = 'Xm90'; % initial gate (currently just use a half power pi pulse... need to rethink how this should be done after calibrations!)
+        gateType = 'Xm90'; % repeated gate type 
         cavityFreq=10.16578e9; % cavity frequency
         cavityAmp=1;       % cavity pulse amplitude
         measDuration = 5e-6;
@@ -34,7 +31,7 @@ classdef X90AmpCal< handle
     end
     
     methods
-        function obj=X90AmpCal()
+        function obj=Xm90AmpCal()
             % constructor generates the necessary objects and calculates the dependent parameters
             obj.initSequences(); % init routine to build gate sequences
             
@@ -56,11 +53,13 @@ classdef X90AmpCal< handle
         function obj=initSequences(obj)
             % generate qubit objects
             obj.iGate= pulselib.singleGate(obj.gateType);
-            obj.iGate.amplitude = obj.iGateAmplitude;
-            obj.iGate.sigma= obj.iGateSigma;
+            obj.iGate.amplitude = obj.qubitAmplitude;
+            obj.iGate.dragAmplitude = obj.qubitDragAmplitude;
+            obj.iGate.sigma= obj.qubitSigma;
             obj.iGate.cutoff = obj.iGateSigma*4;
             obj.mainGate = pulselib.singleGate(obj.gateType);
             obj.mainGate.amplitude = obj.qubitAmplitude;
+            obj.mainGate.dragAmplitude = obj.qubitDragAmplitude;
             obj.mainGate.sigma = obj.qubitSigma;
             obj.mainGate.cutoff = obj.qubitSigma*4;
             
@@ -154,41 +153,6 @@ classdef X90AmpCal< handle
             playlist(backId).segmentAdvance = 'Auto';
         end
         
-%         function w = genWaveset_M8195A(obj)
-%             w = paramlib.M8195A.waveset();
-%             tStep = 1/obj.samplingRate;
-%             t = 0:tStep:(obj.waveformEndTime);
-%             loWaveform = sin(2*pi*obj.cavityFreq*t);
-%             markerWaveform = ones(1,length(t)).*(t>10e-9).*(t<510e-9);
-%             for ind=1:length(obj.sequences)
-%                 s = obj.sequences(ind);
-%                 tStart = obj.sequenceEndTime - s.totalSequenceDuration;
-%                 [iQubitBaseband qQubitBaseband] = s.uwWaveforms(t, tStart);
-%                 iQubitMod=cos(2*pi*obj.qubitFreq*t).*iQubitBaseband;
-%                 qQubitMod=sin(2*pi*obj.qubitFreq*t).*qQubitBaseband;
-%                 [iMeasBaseband qMeasBaseband] = obj.measurement.uwWaveforms(t,obj.measStartTime);
-%                 iMeasMod=cos(2*pi*obj.cavityFreq*t).*iMeasBaseband;
-%                 qMeasMod=sin(2*pi*obj.cavityFreq*t).*qMeasBaseband;
-%                 ch1waveform = iQubitMod+qQubitMod+iMeasMod+qMeasMod;
-%                 % background is measurement pulse to get contrast
-%                 backgroundWaveform = iMeasMod+qMeasMod;
-%                 s1=w.newSegment(ch1waveform,markerWaveform,[1 0; 0 0; 0 0; 0 0]);
-%                 p1=w.newPlaylistItem(s1);
-%                 % create LO segment with same id to play simultaneously
-%                 s2=w.newSegment(loWaveform,markerWaveform,[0 0; 1 0; 0 0; 0 0]);
-%                 s2.id = s1.id;
-%                 % add background to playlist
-%                 sBack = w.newSegment(backgroundWaveform,markerWaveform,[1 0; 0 0; 0 0; 0 0]);
-%                 pBack = w.newPlaylistItem(sBack);
-%                 % add lo to play @ same time as background
-%                 s3=w.newSegment(loWaveform,markerWaveform,[0 0; 1 0; 0 0; 0 0]);
-%                 s3.id = sBack.id;
-%             end
-%             % last playlist item must have advance set to 'auto'
-%             pBack.advance='Auto';
-%         end
-        
-
         function [result] = directRunM8195A(obj,awg,card,cardparams,playlist)
             % some hardware specific settings
             intStart=4000; intStop=8000; % integration times
@@ -241,66 +205,6 @@ classdef X90AmpCal< handle
         end
 
 
-%         function [result] = runExperimentM8195A(obj,awg,card,cardparams)
-%             % integration times
-%             intStart=4000; intStop=8000;
-%             % software averages
-%             softavg=50;
-%             w = obj.genWaveset_M8195A();
-%             WaveLib = awg.WavesetExtractSegmentLibraryStruct(w);
-%             PlayList = awg.WavesetExtractPlaylistStruct(w);
-% %             w.drawSegmentLibrary()
-% %             w.drawPlaylist()
-% %             WaveLib = awg.ApplyCorrection(WaveLib);
-%             awg.Wavedownload(WaveLib);
-%             clear WaveLib;
-%             cardparams.segments=length(w.playlist);
-%             clear w;
-%             cardparams.delaytime=obj.measStartTime-1e-6;
-%             card.SetParams(cardparams);
-%             tstep=1/card.params.samplerate;
-%             taxis=(tstep:tstep:card.params.samples/card.params.samplerate)'./1e-6;%mus units
-%             % READ
-%             % intialize matrices
-%             samples=uint64(cardparams.samples);
-%             Idata=zeros(cardparams.segments/2,samples);
-%             Qdata=zeros(cardparams.segments/2,samples);
-%             Pdata=zeros(cardparams.segments/2,samples);
-% %             phaseData=zeros(cardparams.segments/2,samples); 
-%             for ind=1:softavg
-%                 % "hardware" averaged I,I^2 data
-%                 [tempI,tempI2,tempQ,tempQ2] = card.ReadIandQcomplicated(awg,PlayList);
-%                 % software acumulation
-%                 Idata=Idata+tempI;
-%                 Qdata=Qdata+tempQ;
-% %                 Pdata=Pdata+tempI2+tempQ2;
-%                 Pdata=Idata.^2+Qdata.^2;
-%                 Pint=mean(Pdata(:,intStart:intStop)');
-% %                 phaseData = phaseData + atan(tempQ./tempI);
-% %                 phaseInt = mean(phaseData(:,intStart:intStop)');
-%                 
-%                 if ~mod(ind,10)
-%                     figure(101);
-%                     subplot(2,3,1); imagesc(taxis,obj.numGateVector,Idata/ind);title(['In phase. N=' num2str(ind)]);ylabel('gates');xlabel('Time (\mus)');
-%                     subplot(2,3,2); imagesc(taxis,obj.numGateVector,Qdata/ind);title('Quad phase');ylabel('AmpVector');xlabel('Time (\mus)');
-%                     subplot(2,3,4); imagesc(taxis,obj.numGateVector,Pdata/ind);title('Power I^2+Q^2');ylabel('AmpVector');xlabel('Time (\mus)');
-% %                     subplot(2,3,5); imagesc(taxis,obj.numGateVector,phaseData/ind);title('Phase atan(Q/I)');ylabel('AmpVector');xlabel('Time (\mus)');
-%                     subplot(2,3,3); plot(obj.numGateVector,sqrt(Pint));ylabel('Homodyne Amplitude (V)');xlabel('Qubit pulse software amplitude');
-% %                     subplot(2,3,6); plot(obj.numGateVector,phaseInt);ylabel('Integrated Phase');xlabel('Software Amplitude');
-%                     pause(0.01);
-%                 end
-% %                 ax=subplot(2,2,4);
-%                 % try doing the T1 fit during softaveraging
-% %                 theta = funclib.RabiFit(taxis,Pdata,ax);
-%             end
-%             result.taxis = taxis;
-%             result.Idata=Idata./softavg;
-%             result.Qdata=Qdata./softavg;
-%             result.Pdata=Pdata./softavg;
-%             result.Pint=Pint./softavg;
-%             display('Experiment Finished')
-%             %             result.theta=theta;
-%         end
         
     end
 end
--- +explib/Y90DragCal.m
+++ +explib/Y90DragCal.m
@@ -1,20 +1,18 @@
-classdef X90AmpCal< handle
-    % Pi pulse error amplification. After an initial pi/2 pulse it will
+classdef Y90DragCal< handle
+    % Pi pulse error amplification for drag. After an initial pi/2 pulse it will
     % vary the number of subsequent pi pulses.  
     
     properties
         % change these to tweak the experiment
-        experimentName = 'X90AmpCal';
+        experimentName = 'Y90DragCal';
         numGateVector = 2:2:40;
         qubitFreq=4.772869998748302e9;
-%         qubitAmplitude = .74/2;
-%         iGateAmplitude = .74/2;
-        qubitAmplitude = .5/2;
-        iGateAmplitude = .5/2;
+        qubitAmplitude = .3575;
+%         qubitAmplitude = .7198;
+        qubitDragAmplitude = .015;
         qubitSigma = 25e-9;
-        iGateSigma = 25e-9;
-        iGateType = 'X90'; % initial gate (currently just use a half power pi pulse... need to rethink how this should be done after calibrations!)
-        gateType = 'X90'; % repeated gate type 
+        gateType1 = 'Y90'; % 1st of two opposite gates
+        gateType2 = 'Ym90'; % 
         cavityFreq=10.16578e9; % cavity frequency
         cavityAmp=1;       % cavity pulse amplitude
         measDuration = 5e-6;
@@ -23,8 +21,8 @@ classdef X90AmpCal< handle
         endBuffer = 5e-9; % buffer after measurement pulse
         samplingRate=32e9; % sampling rate
         % these are auto calculated
-        iGate; % initial qubit pulse object
-        mainGate; % qubit pulse object
+        pulse1; % 1st qubit pulse object
+        pulse2; % 1st qubit pulse object
         sequences; % array of gateSequence objects
         measurement; % measurement pulse object
         measStartTime; 
@@ -34,7 +32,7 @@ classdef X90AmpCal< handle
     end
     
     methods
-        function obj=X90AmpCal()
+        function obj=Y90DragCal()
             % constructor generates the necessary objects and calculates the dependent parameters
             obj.initSequences(); % init routine to build gate sequences
             
@@ -55,21 +53,26 @@ classdef X90AmpCal< handle
         
         function obj=initSequences(obj)
             % generate qubit objects
-            obj.iGate= pulselib.singleGate(obj.gateType);
-            obj.iGate.amplitude = obj.iGateAmplitude;
-            obj.iGate.sigma= obj.iGateSigma;
-            obj.iGate.cutoff = obj.iGateSigma*4;
-            obj.mainGate = pulselib.singleGate(obj.gateType);
-            obj.mainGate.amplitude = obj.qubitAmplitude;
-            obj.mainGate.sigma = obj.qubitSigma;
-            obj.mainGate.cutoff = obj.qubitSigma*4;
-            
+            obj.pulse1 = pulselib.singleGate(obj.gateType1);
+            obj.pulse1.amplitude = obj.qubitAmplitude;
+            obj.pulse1.dragAmplitude = obj.qubitDragAmplitude;
+            obj.pulse1.sigma = obj.qubitSigma;
+            obj.pulse1.cutoff = obj.qubitSigma*4;
+            obj.pulse2 = pulselib.singleGate(obj.gateType2);
+            obj.pulse2.amplitude = obj.qubitAmplitude;
+            obj.pulse2.dragAmplitude = obj.qubitDragAmplitude;
+            obj.pulse2.sigma = obj.qubitSigma;
+            obj.pulse2.cutoff = obj.qubitSigma*4;
+
             sequences(1,length(obj.numGateVector)) = pulselib.gateSequence(); % initialize empty array of gateSequence objects
             for ind = 1:length(obj.numGateVector)
                 gateArray(1,obj.numGateVector(ind)) = pulselib.singleGate(); % init empty array of gate objects
-                gateArray(1)=obj.iGate;
                 for ind2 = 1:obj.numGateVector(ind)
-                    gateArray(ind2+1) = obj.mainGate;
+                    if mod(ind2,2) % if odd
+                        gateArray(ind2) = obj.pulse1;
+                    else
+                        gateArray(ind2) = obj.pulse2;
+                    end
                 end
                 sequences(ind)=pulselib.gateSequence(gateArray);
             end
@@ -101,7 +104,7 @@ classdef X90AmpCal< handle
             end
             % create time axis with correct # size
             t = 0:tStep:paddedWaveformEndTime;            
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
             % generate LO and marker waveforms
             loWaveform = sin(2*pi*obj.cavityFreq*t);
             markerWaveform = ones(1,length(t)).*(t>10e-9).*(t<510e-9);
@@ -153,41 +156,6 @@ classdef X90AmpCal< handle
             % last playlist item must have advance set to 'auto'
             playlist(backId).segmentAdvance = 'Auto';
         end
-        
-%         function w = genWaveset_M8195A(obj)
-%             w = paramlib.M8195A.waveset();
-%             tStep = 1/obj.samplingRate;
-%             t = 0:tStep:(obj.waveformEndTime);
-%             loWaveform = sin(2*pi*obj.cavityFreq*t);
-%             markerWaveform = ones(1,length(t)).*(t>10e-9).*(t<510e-9);
-%             for ind=1:length(obj.sequences)
-%                 s = obj.sequences(ind);
-%                 tStart = obj.sequenceEndTime - s.totalSequenceDuration;
-%                 [iQubitBaseband qQubitBaseband] = s.uwWaveforms(t, tStart);
-%                 iQubitMod=cos(2*pi*obj.qubitFreq*t).*iQubitBaseband;
-%                 qQubitMod=sin(2*pi*obj.qubitFreq*t).*qQubitBaseband;
-%                 [iMeasBaseband qMeasBaseband] = obj.measurement.uwWaveforms(t,obj.measStartTime);
-%                 iMeasMod=cos(2*pi*obj.cavityFreq*t).*iMeasBaseband;
-%                 qMeasMod=sin(2*pi*obj.cavityFreq*t).*qMeasBaseband;
-%                 ch1waveform = iQubitMod+qQubitMod+iMeasMod+qMeasMod;
-%                 % background is measurement pulse to get contrast
-%                 backgroundWaveform = iMeasMod+qMeasMod;
-%                 s1=w.newSegment(ch1waveform,markerWaveform,[1 0; 0 0; 0 0; 0 0]);
-%                 p1=w.newPlaylistItem(s1);
-%                 % create LO segment with same id to play simultaneously
-%                 s2=w.newSegment(loWaveform,markerWaveform,[0 0; 1 0; 0 0; 0 0]);
-%                 s2.id = s1.id;
-%                 % add background to playlist
-%                 sBack = w.newSegment(backgroundWaveform,markerWaveform,[1 0; 0 0; 0 0; 0 0]);
-%                 pBack = w.newPlaylistItem(sBack);
-%                 % add lo to play @ same time as background
-%                 s3=w.newSegment(loWaveform,markerWaveform,[0 0; 1 0; 0 0; 0 0]);
-%                 s3.id = sBack.id;
-%             end
-%             % last playlist item must have advance set to 'auto'
-%             pBack.advance='Auto';
-%         end
-        
 
         function [result] = directRunM8195A(obj,awg,card,cardparams,playlist)
             % some hardware specific settings
@@ -224,7 +192,7 @@ classdef X90AmpCal< handle
                     set(h,'Visible','off');
                     someText = {obj.experimentName,['softavg = ' num2str(ind)]};
                     text(.1,.9,someText);
-                    subplot(2,3,2); imagesc(taxis,obj.numGateVector,Idata/ind);title('In phase');ylabel('Number of Gates');xlabel('Time (\mus)');
+                    subplot(2,3,2); imagesc(taxis,obj.numGateVector,Idata/ind);title(['In phase. N=' num2str(ind)]);ylabel('Number of Gates');xlabel('Time (\mus)');
                     subplot(2,3,3); imagesc(taxis,obj.numGateVector,Qdata/ind);title('Quad phase');ylabel('Number of Gates');xlabel('Time (\mus)');
                     subplot(2,3,4); imagesc(taxis,obj.numGateVector,Pdata/ind);title('Power I^2+Q^2');ylabel('Number of Gates');xlabel('Time (\mus)');
                     subplot(2,3,[5 6]); plot(obj.numGateVector,sqrt(Pint));ylabel('Power I^2+Q^2');xlabel('Number of Gates');
@@ -239,69 +207,6 @@ classdef X90AmpCal< handle
             result.Pint=Pint./softavg;
             display('Experiment Finished')
         end
-
-
-%         function [result] = runExperimentM8195A(obj,awg,card,cardparams)
-%             % integration times
-%             intStart=4000; intStop=8000;
-%             % software averages
-%             softavg=50;
-%             w = obj.genWaveset_M8195A();
-%             WaveLib = awg.WavesetExtractSegmentLibraryStruct(w);
-%             PlayList = awg.WavesetExtractPlaylistStruct(w);
-% %             w.drawSegmentLibrary()
-% %             w.drawPlaylist()
-% %             WaveLib = awg.ApplyCorrection(WaveLib);
-%             awg.Wavedownload(WaveLib);
-%             clear WaveLib;
-%             cardparams.segments=length(w.playlist);
-%             clear w;
-%             cardparams.delaytime=obj.measStartTime-1e-6;
-%             card.SetParams(cardparams);
-%             tstep=1/card.params.samplerate;
-%             taxis=(tstep:tstep:card.params.samples/card.params.samplerate)'./1e-6;%mus units
-%             % READ
-%             % intialize matrices
-%             samples=uint64(cardparams.samples);
-%             Idata=zeros(cardparams.segments/2,samples);
-%             Qdata=zeros(cardparams.segments/2,samples);
-%             Pdata=zeros(cardparams.segments/2,samples);
-% %             phaseData=zeros(cardparams.segments/2,samples); 
-%             for ind=1:softavg
-%                 % "hardware" averaged I,I^2 data
-%                 [tempI,tempI2,tempQ,tempQ2] = card.ReadIandQcomplicated(awg,PlayList);
-%                 % software acumulation
-%                 Idata=Idata+tempI;
-%                 Qdata=Qdata+tempQ;
-% %                 Pdata=Pdata+tempI2+tempQ2;
-%                 Pdata=Idata.^2+Qdata.^2;
-%                 Pint=mean(Pdata(:,intStart:intStop)');
-% %                 phaseData = phaseData + atan(tempQ./tempI);
-% %                 phaseInt = mean(phaseData(:,intStart:intStop)');
-%                 
-%                 if ~mod(ind,10)
-%                     figure(101);
-%                     subplot(2,3,1); imagesc(taxis,obj.numGateVector,Idata/ind);title(['In phase. N=' num2str(ind)]);ylabel('gates');xlabel('Time (\mus)');
-%                     subplot(2,3,2); imagesc(taxis,obj.numGateVector,Qdata/ind);title('Quad phase');ylabel('AmpVector');xlabel('Time (\mus)');
-%                     subplot(2,3,4); imagesc(taxis,obj.numGateVector,Pdata/ind);title('Power I^2+Q^2');ylabel('AmpVector');xlabel('Time (\mus)');
-% %                     subplot(2,3,5); imagesc(taxis,obj.numGateVector,phaseData/ind);title('Phase atan(Q/I)');ylabel('AmpVector');xlabel('Time (\mus)');
-%                     subplot(2,3,3); plot(obj.numGateVector,sqrt(Pint));ylabel('Homodyne Amplitude (V)');xlabel('Qubit pulse software amplitude');
-% %                     subplot(2,3,6); plot(obj.numGateVector,phaseInt);ylabel('Integrated Phase');xlabel('Software Amplitude');
-%                     pause(0.01);
-%                 end
-% %                 ax=subplot(2,2,4);
-%                 % try doing the T1 fit during softaveraging
-% %                 theta = funclib.RabiFit(taxis,Pdata,ax);
-%             end
-%             result.taxis = taxis;
-%             result.Idata=Idata./softavg;
-%             result.Qdata=Qdata./softavg;
-%             result.Pdata=Pdata./softavg;
-%             result.Pint=Pint./softavg;
-%             display('Experiment Finished')
-%             %             result.theta=theta;
-%         end
-        
     end
 end
        
