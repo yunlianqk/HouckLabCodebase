b149e3e2f72688b90a23fcee9a456a512425dbae
BF1 <houcklabprinceton@gmail.com>
2016-09-27 10:41:05 -0400

    Gengyan - Enhance YOGOGS200 class Add set/get current method Use auto range instead of fixed range Add ShowError method

--- @YOKOGS200/GetVoltage.m
+++ @YOKOGS200/GetVoltage.m
@@ -1,5 +1,6 @@
 function voltage = GetVoltage(yoko)
 % Get voltage
+    fprintf(yoko.instrhandle, 'SOURce:FUNCtion VOLTage');
     fprintf(yoko.instrhandle, ':SOURce:LEVel?');
     voltage = fscanf(yoko.instrhandle, '%f');
 end
