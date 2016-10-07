6cde626188b602f0668824ef3a846f546bd4f80f
BF1 <houcklabprinceton@gmail.com>
2016-10-07 11:08:30 -0400

    Gengyan - Add some generic methods to GPIB instrument Add 'Query' and 'GetReply' methods

--- @GPIBINSTR/GPIBINSTR.m
+++ @GPIBINSTR/GPIBINSTR.m
@@ -12,10 +12,10 @@ classdef GPIBINSTR < handle
         function self = GPIBINSTR(address)
             %  Open instrument
             
-            % If already opened, do nothing
+            % If already opened, attach the handle to self
             self.instrhandle = instrfind('Name', ['GPIB0-', num2str(address)], ...
                                          'Status', 'open');
-            % If not opened, open it
+            % If not opened, open it and attach handle to self
             if isempty(self.instrhandle)
                 self.instrhandle = gpib('ni', 0, address);
                 fopen(self.instrhandle);
@@ -27,6 +27,7 @@ classdef GPIBINSTR < handle
         function Finalize(self)
             % Close instrhandle            
             if strcmp(self.instrhandle.Status, 'open')
+                clrdevice(self.instrhandle);  % Clear input and output buffer
                 fclose(self.instrhandle);
             end
             display([class(self), ' object finalized.']);
@@ -34,15 +35,29 @@ classdef GPIBINSTR < handle
         
         function SendCommand(self, command)
             % Send low-level command to instrument
-            if strcmp(self.instrhandle.Status, 'open')
-                fprintf(self.instrhandle, command);
-            else
-                display('Instrument is not open');
-            end
+            fprintf(self.instrhandle, command);
         end
         
+        function s = GetReply(self)
+            % Read data from instrument output buffer
+            s = fscanf(self.instrhandle, '%s');
+        end
+        
+        function s = Query(self, command)
+            % Send low-level inquiry to instrument and get reply
+            % Same as SendCommand + GetReply
+            if ~ismember('?', command)
+                % If command doesn't contain '?', throw warning
+                if ~strcmp(input('command does not contain ''?''. Are you sure (y/N)? ', 's'), 'y')
+                    return;
+                end
+            end
+            s = query(self.instrhandle, command);
+        end
+        
+
         function s = Info(self)
-            % Display and return instrument info
+            % Return instrument info
             fprintf(self.instrhandle, '*IDN?');
             s = fscanf(self.instrhandle, '%s');
         end
