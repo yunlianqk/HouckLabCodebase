ea8a5ab3c318f3c20ef5b3de6140e0c956d83040
houcklab <houcklabprinceton@gmail.com>
2016-10-26 16:11:54 -0400

    Mattias - Add USB-GPIB support For Keysight embedded controller

--- instruments_initialize_oxford.m
+++ instruments_initialize_oxford.m
@@ -1,27 +1,13 @@
 % Initialize instruments
-path = pwd;
-addpath(genpath(path));
-
-address = struct('rfgen', 20, ...
-                 'specgen', 24, ...
-                 'logen', 23, ...
-                 'pnax', 16, ...
-                 'yoko1', 2, ...
-                 'yoko2', 3, ...
-                 'yoko3', 4)
-%                  'triggen', 9)
-%                  'card', 'PXI7::4::0::INSTR', 
-%                  'pulsegen', 'PXI50::15::0::INSTR')
-
-global rfgen;
-rfgen = E8267DGenerator(address.rfgen);
-
-global specgen;
-specgen = E8267DGenerator(address.specgen);
-
-global logen;
-logen = E8267DGenerator(address.logen);
-
+% run('.\setpath.m');
+addpath('C:\Users\Administrator\Documents\GitHub\HouckLabMeasurementCode\JJR\TunableDimer');
+addpath('C:\Users\Administrator\Documents\GitHub\HouckLabMeasurementCode');
+             
+address = struct('pnax', 'GPIB0::16::INSTR', ...
+                 'yoko1', 'GPIB0::2::INSTR', ...
+                 'yoko2', 'GPIB0::3::INSTR', ...
+                 'yoko3', 'GPIB0::4::INSTR');
+ 
 global pnax;
 pnax = PNAXAnalyzer(address.pnax);
 
@@ -34,13 +20,34 @@ yoko2 = YOKO7651(address.yoko2);
 global yoko3;
 yoko3 = YOKOGS200(address.yoko3);
 
-% global triggen;
-% triggen = AWG33250A(address.triggen);
+global M8195AWG
+M8195AWG = M8195AWG();
+
+% global pxa;
+% pxa = PXAAnalyzer(address.pxa);
+
+% global rfgen;
+% rfgen = E8267DGenerator(address.rfgen);
+
+% global specgen;
+% specgen = E8267DGenerator(address.specgen);
+
+% global logen;
+% logen = E8267DGenerator(address.logen);
+
+% global card;
+% card = U1084ADigitizer(address.card);
+% 
+% global pulsegen;
+% pulsegen = M9330AWG(address.pulsegen);
+
+% global card;
+% card = U1084ADigitizer(address.card);
 
 % global card;
 % card = U1082ADigitizer(address.card);
 
-% global pulsegen;
-% pulsegen = M9330AWG(address.pulsegen);
+% global triggen;
+% triggen = AWG33250A(address.triggen);
 
 clear('address');
