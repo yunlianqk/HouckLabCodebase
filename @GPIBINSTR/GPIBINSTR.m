6d3604e4c96c2b4923e4921b2d6b417c8857c077
BF1 <houcklabprinceton@gmail.com>
2016-10-27 09:28:51 -0400

    Gengyan - Improve GPIBINSTR class Use visa instead of gpib in MATLAB to support both GPIB card and GPIB-USB adapter.

--- @GPIBINSTR/GPIBINSTR.m
+++ @GPIBINSTR/GPIBINSTR.m
@@ -12,44 +12,76 @@ classdef GPIBINSTR < handle
         function self = GPIBINSTR(address)
             %  Open instrument
             
-            if isa(address,'double')
-                % If already opened, do nothing
-                self.instrhandle = instrfind('Name', ['GPIB0-', num2str(address)], ...
-                    'Status', 'open');
-                % If not opened, open it
-                if isempty(self.instrhandle)
-                    self.instrhandle = gpib('ni', 0, address);
-                    fopen(self.instrhandle);
+            % Extract integer primary GPIB address from full address string
+            instrID = sscanf(address, 'GPIB0::%d::0::INSTR');
+            % If already open, attach handle to self
+            self.instrhandle = instrfind('Name', ['VISA-GPIB0-', num2str(instrID)], ...
+                                         'Status', 'open');
+            % If closed, open it and attach handle to self
+            if isempty(self.instrhandle)
+                % Try different vendors
+                vendorlist = {'ni', 'agilent'};
+                for vendor = vendorlist
+                    try
+                        % Use 'visa' instead of 'gpib' to support both
+                        % GPIB card and GPIB-USB adapter
+                        self.instrhandle = visa(vendor{1}, address);
+                        fopen(self.instrhandle);
+                        fprintf(self.instrhandle, '*IDN?');
+                        fscanf(self.instrhandle, '%s');
+                        break;
+                    catch
+                    end
+                    error([class(self), ' object initialization failed. ', ...
+                          'Address: ', num2str(address)]);
                 end
-            else
-                
-                self.instrhandle = instrfind('Name', ['VISA-GPIB0-', num2str(sscanf(address, 'GPIB0::%d::INSTR'))], ...
-                    'Status', 'open');
-                if isempty(self.instrhandle)
-                    self.instrhandle = visa('agilent', address);
-                    fopen(self.instrhandle);
-                end
+
             end
-            
             self.address = address;
+            display([class(self), ' object created.']);
         end
         
         function Finalize(self)
             % Close instrhandle            
             if strcmp(self.instrhandle.Status, 'open')
+                clrdevice(self.instrhandle);  % Clear input and output buffer
                 fclose(self.instrhandle);
             end
+            display([class(self), ' object finalized.']);
         end
         
         function SendCommand(self, command)
             % Send low-level command to instrument
-            if strcmp(self.instrhandle.Status, 'open')
-                fprintf(self.instrhandle, command);
-            else
-                display('Instrument is not open');
+            fprintf(self.instrhandle, command);
+        end
+        
+        function s = GetReply(self)
+            % Read data from instrument output buffer
+            s = fscanf(self.instrhandle, '%s');
+        end
+        
+        function s = Query(self, command)
+            % Send low-level inquiry to instrument and get reply
+            % Same as SendCommand + GetReply
+            if ~ismember('?', command)
+                % If command doesn't contain '?', throw warning
+                if ~strcmp(input('command does not contain ''?''. Are you sure (y/N)? ', 's'), 'y')
+                    return;
+                end
             end
+            s = query(self.instrhandle, command);
+        end
+
+        function s = Info(self)
+            % Return instrument info
+            fprintf(self.instrhandle, '*IDN?');
+            s = fscanf(self.instrhandle, '%s');
+        end
+        
+        function Reset(self)
+            % Reset instrument
+            fprintf(self.instrhandle, '*RST');
         end
     end
-    
 end
 
