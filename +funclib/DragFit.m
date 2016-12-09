00c1a798d92e87af15d0cf92a688d6327cd4e8af
houcklab <houcklabprinceton@gmail.com>
2016-12-09 17:22:30 -0500

    Gengyan - Update funclib Better fit for DRAG Merge RabiFit and RabiFit2 Merge ExpFit, ExpFit2 and ExpFit3

--- +funclib/DragFit.m
+++ +funclib/DragFit.m
@@ -1,5 +1,6 @@
 function  newDragAmplitude = DragFit(axis, data, varargin)
-% Fit -cos function for drag vs amplitude - for explib.  
+% Fit -cos function for drag vs amplitude
+
 % free parameters are offset, amplitude, frequency, and phase
 % outputs the amplitude corresponding to the cosine minimum nearest zero
 
@@ -10,20 +11,18 @@ function  newDragAmplitude = DragFit(axis, data, varargin)
     amp_guess = dataMax-dataMin;
     freq_guess = 0.5/abs(axis(maxInd)-axis(minInd));
     phase_guess = 0;
-    beta0 = [offset_guess amp_guess freq_guess phase_guess];
+    beta0 = [offset_guess, amp_guess, freq_guess, phase_guess];
     % Fit data
-%     coeff = nlinfit(axis, data, @drag, beta0);
     Lbound =[0, 0, 0, -pi];   % lower bounds on coeff
     Ubound =[1, 2*amp_guess, abs(freq_guess), pi];    % upper bounds on coeff
     opts = optimset('Display', 'off'); % suppress fit message 
     coeff = lsqcurvefit(@drag, beta0, axis, data, Lbound, Ubound, opts);
-    
-    freqFit = coeff(3);
-    phaseFit = coeff(4);
-    newDragAmplitude = -phaseFit/(2*pi*freqFit);
+
     % Plot original and fitted data
     axis_dense = linspace(axis(1), axis(end), 1000);
     Y = drag(coeff, axis_dense);
+    [~, index] = min(Y);
+    newDragAmplitude = axis_dense(index);
     
     if ~isempty(varargin) && ishandle(varargin{1})
         ax = varargin{1};
@@ -37,7 +36,7 @@ function  newDragAmplitude = DragFit(axis, data, varargin)
 end
 
 function y = drag(beta, x)
-    offset=beta(1);
+    offset = beta(1);
     amp = beta(2);
     freq = beta(3);
     phase = beta(4);
--- +funclib/ExpFit.m
+++ +funclib/ExpFit.m
@@ -1,14 +1,24 @@
-function  lamda = ExpFit(axis, data, varargin)
+function  result = ExpFit(axis, data, varargin)
 % Exponential fit
 
     % Construct initial guess for parameters
     offset_guess = data(end);
-    amp_guess = data(1)-data(end);
-    lambda_guess = -axis(20)/log(1-(data(1)-data(20))/amp_guess);
-    beta0 = [amp_guess, lambda_guess, offset_guess];
+    amp_guess = data(1) - data(end);
+    [~, index] = min(abs(data - data(1) - amp_guess*(exp(-1)-1)));
+    lambda_guess = axis(index); % should make it easier for nonuniform fits
+    beta0 = [amp_guess, lambda_guess,offset_guess];
+    
     % Fit data
-    coeff = nlinfit(axis, data, @Exp_beta, beta0);
-    lamda = coeff(2);
+    [coeff,R,J,CovB,MSE,ErrorModelInfo] =  nlinfit(axis, data, @Exp_beta, beta0);
+    result.amp=coeff(1);
+    result.lambda=coeff(2);
+    result.offset=coeff(3);
+    result.R=R;
+    result.J=J;
+    result.CovB=CovB;
+    result.MSE=MSE;
+    result.ErrorModelInfo=ErrorModelInfo;
+    
     % Plot original and fitted data
     axis_dense = linspace(axis(1), axis(end), 1000);
     Y = Exp_beta(coeff, axis_dense);
@@ -30,5 +40,3 @@ function y = Exp_beta(beta, x)
     offset = beta(3);
     y = offset+amp*exp(-x/lambda);
 end
-
-
