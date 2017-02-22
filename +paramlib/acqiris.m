5c187a10bcd037824377a3b48d5d300bfbf6e907
BF1 <houcklabprinceton@gmail.com>
2017-02-22 10:46:19 -0500

    Gengyan - Update paramlib.acqiris Remove softAvg property

--- +paramlib/acqiris.m
+++ +paramlib/acqiris.m
@@ -7,7 +7,6 @@ classdef acqiris
         sampleinterval = 1e-9; % Sampling interval in seconds, from 1 ns to 0.1 ms in 1, 2, 2.5, 4, 5 sequence
         samples = 10000; % Number of samples for each segment, from 16 to 2 Mega (2^21) in steps of 16    
         averages = 30000; % Number of averages, from 1 to 65536
-        softAvg = 1;
         segments = 1; % Number of segments, from 1 to 8191
         delaytime = 1e-6; % Delay time from trigger to start of acquistion
         couplemode = 'DC'; % Coupling mode, 'AC' or 'DC'
