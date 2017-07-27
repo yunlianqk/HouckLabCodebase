0e3e6271b74dd571517a6e8c0d44da31b00f9788
Pranav <pranavm1502@gmail.com>
2017-07-27 18:51:30 -0400

    Pranav - Reworked Plot.m
    Now it includes tomography plots.

--- +measlib/@SmartSweep/Plot.m
+++ +measlib/@SmartSweep/Plot.m
@@ -11,31 +11,17 @@ function Plot(self, fignum)
 
     figure(fignum);
     if self.histogram
-        numRows = size(self.result.intI, 1);
-        self.result.countsI = zeros(numRows, self.histbins);
-        self.result.countsQ = zeros(numRows, self.histbins);
-        self.result.edgesI = linspace(min(min(self.result.intI)), ...
-                                      max(max(self.result.intI)), ...
-                                      self.histbins+1);
-        self.result.edgesQ = linspace(min(min(self.result.intQ)), ...
-                                      max(max(self.result.intQ)), ...
-                                      self.histbins+1);
-        for row = 1:numRows
-            [self.result.countsI(row, :), ~] ...
-                = histcounts(self.result.intI(row, :), self.result.edgesI);
-            [self.result.countsQ(row, :), ~] ...
-                = histcounts(self.result.intQ(row, :), self.result.edgesQ);
-        end
-        subplot(2, 1, 1);
-        plot(self.result.edgesI(2:end), self.result.countsI);
-        xlabel('I data');
-        ylabel('Counts');
-        legend(cellstr(num2str((1:numRows)')));
-        subplot(2, 1, 2);
-        plot(self.result.edgesI(2:end), self.result.countsQ);
-        xlabel('Q data');
-        ylabel('Counts');
+        histPlot(self);
     else
+        % Plot Raw and integrated data
+        dataPlot(self);
+        % Plot tomography data
+        TomoPlot(self,fignum);
+    end
+    drawnow;
+end
+
+function dataPlot(self)
         numRows = size(self.result.dataI, 1);
         % Plot raw data
         subplot(2, 2, 1);
@@ -71,6 +57,72 @@ function Plot(self, fignum)
         plot(1:numRows, self.result.intQ);
         title('Integrated Q');
         axis tight;
-    end
-    drawnow;
+
+
+end
+function histPlot(self)
+        numRows = size(self.result.intI, 1);
+        self.result.countsI = zeros(numRows, self.histbins);
+        self.result.countsQ = zeros(numRows, self.histbins);
+        self.result.edgesI = linspace(min(min(self.result.intI)), ...
+                                      max(max(self.result.intI)), ...
+                                      self.histbins+1);
+        self.result.edgesQ = linspace(min(min(self.result.intQ)), ...
+                                      max(max(self.result.intQ)), ...
+                                      self.histbins+1);
+        for row = 1:numRows
+            [self.result.countsI(row, :), ~] ...
+                = histcounts(self.result.intI(row, :), self.result.edgesI);
+            [self.result.countsQ(row, :), ~] ...
+                = histcounts(self.result.intQ(row, :), self.result.edgesQ);
+        end
+        subplot(2, 1, 1);
+        plot(self.result.edgesI(2:end), self.result.countsI);
+        xlabel('I data');
+        ylabel('Counts');
+        legend(cellstr(num2str((1:numRows)')));
+        subplot(2, 1, 2);
+        plot(self.result.edgesI(2:end), self.result.countsQ);
+        xlabel('Q data');
+        ylabel('Counts');
+end
+
+function TomoPlot(self,fignum)
+            % Plot tomography
+            figure(fignum+1);
+            plot(1:numTomoGates, self.result.TomoAmp, '-o');
+            set(gca, 'xtick', 1:numTomoGates);
+            set(gca, 'xticklabel', {'Id','X90','Y90'});
+            set(gca, 'xticklabelrotation', 45);
+            axis([0, numTomoGates+1, -0.2, 1.2]);
+            plotlib.hline(1);
+            plotlib.hline(0);
+            plotlib.hline(0.5);
+            hold on;
+            for ind = 1:numTomoGates
+                plot([ind, ind], [-0.2, self.result.TomoAmp(ind)], 'r:');
+            end
+            hold off;
+            title([self.name,'Q1Tomography']);
+            ylabel('P(|0>)');
+			drawnow;
+            if length(self.cardchannel) == 2
+                figure(fignum+2);
+                plot(1:numTomoGates, self.result.TomoAmp2, '-o');
+                set(gca, 'xtick', 1:numTomoGates);
+                set(gca, 'xticklabel', {'Id','X90','Y90'});
+                set(gca, 'xticklabelrotation', 45);
+                axis([0, numTomoGates+1, -0.2, 1.2]);
+                plotlib.hline(1);
+                plotlib.hline(0);
+                plotlib.hline(0.5);
+                hold on;
+                for ind = 1:numTomoGates
+                    plot([ind, ind], [-0.2, self.result.TomoAmp2(ind)], 'r:');
+                end
+                hold off;
+                title([self.name,'_Q2Tomography']);
+                ylabel('P(|0>)');
+                drawnow;
+            end
 end
